223abc66a9bb242f5cefb4ae214efcbcd8b5dcac
PokerLee <1048402825@qq.com>
2024-09-02 16:23:23 +0800

    Sanding abs-encoder pmsm motor position control for version 1.0

---
+++ Sanding_PMSMControl_HX0025EDC_v1.0/Debug/haawking-drivers/haawking-dsc280025_edc-board/common/crt.d
@@ -0,0 +1,5 @@
+haawking-drivers/haawking-dsc280025_edc-board/common/crt.o: \
+  ..\haawking-drivers\haawking-dsc280025_edc-board\common\crt.S \
+  ..\haawking-drivers\haawking-dsc280025_edc-board\common\encoding.h
+
+..\haawking-drivers\haawking-dsc280025_edc-board\common\encoding.h:
---
+++ Sanding_PMSMControl_HX0025EDC_v1.0/Debug/haawking-drivers/haawking-dsc280025_edc-board/sci.d
@@ -0,0 +1,20 @@
+haawking-drivers/haawking-dsc280025_edc-board/sci.o: \
+  ..\haawking-drivers\haawking-dsc280025_edc-board\sci.c \
+  ..\haawking-drivers\haawking-dsc280025_edc-board\sci.h \
+  ..\haawking-drivers\haawking-dsc280025_edc-board\inc\hw_memmap.h \
+  ..\haawking-drivers\haawking-dsc280025_edc-board\inc\hw_sci.h \
+  ..\haawking-drivers\haawking-dsc280025_edc-board\inc\hw_types.h \
+  ..\haawking-drivers\haawking-dsc280025_edc-board\common\hx_rv32_type.h \
+  ..\haawking-drivers\haawking-dsc280025_edc-board\debug.h
+
+..\haawking-drivers\haawking-dsc280025_edc-board\sci.h:
+
+..\haawking-drivers\haawking-dsc280025_edc-board\inc\hw_memmap.h:
+
+..\haawking-drivers\haawking-dsc280025_edc-board\inc\hw_sci.h:
+
+..\haawking-drivers\haawking-dsc280025_edc-board\inc\hw_types.h:
+
+..\haawking-drivers\haawking-dsc280025_edc-board\common\hx_rv32_type.h:
+
+..\haawking-drivers\haawking-dsc280025_edc-board\debug.h:
